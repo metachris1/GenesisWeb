85d5db59d0bc66f5155c0839b18f6ee4e371e076
ANTOINE <ANTOINE>
2017-04-15 16:13:08 +0200

    add test file for vieo player

--- testEpisodeContent.php
+++ testEpisodeContent.php
@@ -14,7 +14,7 @@ require_once("php/classMain.php");
 $dataEpisode = new Episode();
 $dataEpisode->title = "Hello episode";
 $dataEpisode->description = "The best episode of the best show you've ever seen!";
-$dataEpisode->image = "resources/tempEpisodeImage.png";
+$dataEpisode->video = "resources/SWAG.mp4";
 
 $guiEpisode = new GUI_EpisodeBig($dataEpisode);
 
